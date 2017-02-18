afdb9ae4b05390dd9b1d493a31ba9b8ef3cc9d9d
Josh <peterson.joshua.j@gmail.com>
2017-02-18 10:11:17 -0500

    Data Aquisition MVC Initialized

---
+++ Instrument_Data_Aquirer/Social_Stocks_Instrument_/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Social_Stocks_Instrument_.MvcApplication" Language="C#" %>
---
+++ Instrument_Data_Aquirer/Social_Stocks_Instrument_/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Social_Stocks_Instrument_.Startup))]
+namespace Social_Stocks_Instrument_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
