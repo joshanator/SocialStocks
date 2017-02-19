6b7fc00216caea4a7fece0fae9f3e970729e1540
Josh <peterson.joshua.j@gmail.com>
2017-02-19 02:38:56 -0500

    Additional Controller Methods

--- SocialStocksWebAPI/SocialStocksWebAPI/Controllers/UserController.cs
+++ SocialStocksWebAPI/SocialStocksWebAPI/Controllers/UserController.cs
@@ -34,7 +34,53 @@ namespace SocialStocksWebAPI.Controllers
             userData.user = user;
 
             return userData.userTweetList;
-
         }
+
+        public List<Models.tweetsByUser> Get(string symbol, string user, string keyword, DateTime start)
+        {
+            Models.byUser userData = new Models.byUser();
+            List<Models.StockInfo> stockData = new List<Models.StockInfo>();
+            List<Models.tweetsByUserSolo> soloData = new List<Models.tweetsByUserSolo>();
+
+            using (System.Net.WebClient web = new WebClient())
+            {
+                string userDataUrl = "http://socialstocks.us-west-2.elasticbeanstalk.com/TwitterSearch/search2.php?"
+                + "user=" + user + "&q=" + keyword;
+                string tData = web.DownloadString(userDataUrl);
+                Models.byUserSolo s = Models.byUserConstructor.Parse(tData);
+                soloData = s.justTweets;
+                userData = Models.byUserConstructor.Parse(soloData, start, symbol);
+            }
+            userData.keyword = keyword;
+            userData.symbol = symbol;
+            userData.user = user;
+
+            return userData.userTweetList;
+        }
+
+        public List<Models.tweetsByUser> Get(string symbol, string user, string keyword, DateTime start, DateTime end)
+        {
+            Models.byUser userData = new Models.byUser();
+            List<Models.StockInfo> stockData = new List<Models.StockInfo>();
+            List<Models.tweetsByUserSolo> soloData = new List<Models.tweetsByUserSolo>();
+
+            using (System.Net.WebClient web = new WebClient())
+            {
+                string userDataUrl = "http://socialstocks.us-west-2.elasticbeanstalk.com/TwitterSearch/search2.php?"
+                + "user=" + user + "&q=" + keyword;
+                string tData = web.DownloadString(userDataUrl);
+                Models.byUserSolo s = Models.byUserConstructor.Parse(tData);
+                soloData = s.justTweets;
+
+                userData = Models.byUserConstructor.Parse(soloData, start, end, symbol);
+            }
+            userData.keyword = keyword;
+            userData.symbol = symbol;
+            userData.user = user;
+
+            return userData.userTweetList;
+        }
+
+
     }
 }
--- SocialStocksWebAPI/SocialStocksWebAPI/Models/byUser.cs
+++ SocialStocksWebAPI/SocialStocksWebAPI/Models/byUser.cs
@@ -102,6 +102,73 @@ namespace SocialStocksWebAPI.Models
             return final;
         }
 
+        public static byUser Parse(List<tweetsByUserSolo> tList, DateTime start, DateTime end, string symbol)
+        {
+            byUser final = new byUser();
+            List<tweetsByUser> finalList = new List<tweetsByUser>();
+            Models.StockList stocks = new Models.StockList();
+
+            string Symbol = symbol;
+            DateTime startDate = start;
+            DateTime endDate = end;
+
+            string csvData;
+            using (System.Net.WebClient web = new WebClient())
+            {
+                string downloadString = "http://chart.finance.yahoo.com/table.csv?s="
+                    + Symbol
+                    + "&a=" + (startDate.Month - 1) + "&b=" + startDate.Day + "&c=" + startDate.Year
+                    + "&d=" + (endDate.Month - 1) + "&e=" + endDate.Day + "&f=" + endDate.Year
+                    + "&g=d&ignore=.csv";
+
+                csvData = web.DownloadString(downloadString);
+
+                stocks = Models.YahooFinance.Parse(csvData, Symbol, startDate, endDate);
+            }
+
+            DateTime cur = start;
+            while (cur <= DateTime.Now)
+            {
+                tweetsByUser temp = new tweetsByUser();
+                temp.date = cur;
+
+                foreach (Models.StockInfo s in stocks.dataList)
+                {
+                    if (s.date == cur)
+                    {
+                        temp.price = s.price;
+                    }
+                }
+                foreach (tweetsByUserSolo t in tList)
+                {
+                    if (t.date == cur)
+                    {
+                        temp.count = t.count;
+                    }
+                }
+                cur = cur.AddDays(1);
+
+                finalList.Add(temp);
+            }
+
+            for (int i = 0; i < finalList.Count; i++)
+            {
+                if (finalList[i].price == 0 && i > 0)
+                {
+                    finalList[i].price = finalList[i - 1].price;
+                }
+                else if (finalList[i].price == 0 && i == 0)
+                {
+                    finalList.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            final.userTweetList = finalList;
+            return final;
+        }
+
+
         public static byUserSolo Parse(string data)
         {
             byUserSolo justTweetsList = new byUserSolo();
